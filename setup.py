b792baf3fcbd3115dee0b8d5b0da5868aa82965c
Jakub Boukal <SukiCZ@users.noreply.github.com>
2024-10-27 09:43:47 +0100

    Allow Pillow to be any version >= 7.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-requirements = ['Pillow>=7.0,<11']
+requirements = ['Pillow>=7.0']
 
 setup_requirements = []
 
